2779d9913c7ac26c557417ff6492ac5505ad876b
Ashley Joyce <ashleyjoyce72@gmail.com>
2016-01-09 21:13:24 +1100

    My Plots for Assignment 1
    These plots demonstrate the base graphics package in R

---
+++ plot2.R
@@ -0,0 +1,71 @@
+# plot2.R - Assignment 1 of Exploratory Data Analyis Course
+# Author - Ashley Joyce
+# Dataset has come from the UCI Machine Learning Repository
+#http://archive.ics.uci.edu/ml/
+# set working directory (based on your working directory)
+working.dir <- getwd()
+setwd(working.dir)
+
+# read required libraries
+library(data.table)
+
+# make sure the local data folder exists
+if (!file.exists('local data')) {
+  dir.create('local data')
+}
+
+# check to see if the existing data set exists; if not, create it...
+if (!file.exists('local data/power_consumption.txt')) {
+  
+  # download the zip file and unzip into local data
+  file.url<-'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
+  download.file(file.url,destfile='local data/power_consumption.zip')
+  unzip('local data/power_consumption.zip',exdir='local data',overwrite=TRUE)
+  
+  # When using fread - Dates are read as character currently. They can be converted afterwards using as.Date()
+  dt <- fread('local data/household_power_consumption.txt', sep = ";", header=TRUE,data.table=TRUE,na.strings=c("NA","?", ""), verbose=FALSE)
+  
+  # Convert the Date column to "date" class & subset between 01/02/2007 & 02/02/2007
+  dt.2007 <- subset(dt, as.Date(dt$Date,"%d/%m/%Y") %in% as.Date("2007/02/01"):as.Date("2007/02/02"))
+  
+  # Paste Date to Time Cols and flip around so its in the correct format for the strptime()
+  
+  DateTime <- paste(as.Date(dt.2007$Date, format="%d/%m/%Y"),dt.2007$Time, sep=" ")
+  DateTime <- strptime(DateTime, format="%Y-%m-%d %H:%M:%S")
+  
+  #  Bind DateTime Column to dataframe dt.2007
+  dt.2007 <- cbind(dt.2007,DateTime=c(as.POSIXct(DateTime)))
+  
+  # write a clean data set to the directory
+  
+  write.table(dt.2007,file='local data/power_consumption.txt',sep=',',row.names=FALSE)
+  
+} else {
+  
+  dt.2007 <- read.table('local data/power_consumption.txt',header=TRUE,sep=',')
+  dt.2007$DateTime<-as.POSIXlt(dt.2007$DateTime)
+  
+}
+
+# delete the source data set 
+if (file.exists('local data/household_power_consumption.txt')) {
+  x<-file.remove('local data/household_power_consumption.txt')
+}
+
+
+## Plot 2 - line Graph of Global Active Power (kilowatts) over two days.
+
+# open device
+dev.copy(png, file = "plot2.png", width = 480, height = 480, units='px') # Copy my plot to a PNG file 
+
+# Plot line graph 
+with(dt.2007, plot(dt.2007$DateTime,dt.2007$Global_active_power, ylab = "Global Active Power (kilowatts)", xlab="",type = "l"))
+
+#close the PDF file device # Close the PNG device
+dev.off()  
+
+
+
+
+
+
---
+++ plot3.R
@@ -0,0 +1,76 @@
+# plot3.R- Assignment 1 of Exploratory Data Analyis Course
+# Author - Ashley Joyce
+# Dataset has come from the UCI Machine Learning Repository
+#http://archive.ics.uci.edu/ml/
+# set working directory (based on your working directory)
+working.dir <- getwd()
+setwd(working.dir)
+
+# read required libraries
+library(data.table)
+
+# make sure the local data folder exists
+if (!file.exists('local data')) {
+  dir.create('local data')
+}
+
+# check to see if the existing data set exists; if not, create it...
+if (!file.exists('local data/power_consumption.txt')) {
+
+   # download the zip file and unzip into local data
+  file.url<-'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
+  download.file(file.url,destfile='local data/power_consumption.zip')
+  unzip('local data/power_consumption.zip',exdir='local data',overwrite=TRUE)
+
+  # When using fread - Dates are read as character currently. They can be converted afterwards using as.Date()
+  dt <- fread('local data/household_power_consumption.txt', sep = ";", header=TRUE,data.table=TRUE,na.strings=c("NA","?", ""), verbose=FALSE)
+
+  # Convert the Date column to "date" class & subset between 01/02/2007 & 02/02/2007
+  dt.2007 <- subset(dt, as.Date(dt$Date,"%d/%m/%Y") %in% as.Date("2007/02/01"):as.Date("2007/02/02"))
+
+  # Paste Date to Time Cols and flip around so its in the correct format for the strptime()
+
+  DateTime <- paste(as.Date(dt.2007$Date, format="%d/%m/%Y"),dt.2007$Time, sep=" ")
+  DateTime <- strptime(DateTime, format="%Y-%m-%d %H:%M:%S")
+              
+#  Bind DateTime Column to dataframe dt.2007
+  dt.2007 <- cbind(dt.2007,DateTime=c(as.POSIXct(DateTime)))
+
+  # write a clean data set to the directory
+
+  write.table(dt.2007,file='local data/power_consumption.txt',sep=',',row.names=FALSE)
+
+} else {
+
+  dt.2007 <- read.table('local data/power_consumption.txt',header=TRUE,sep=',')
+  dt.2007$DateTime<-as.POSIXlt(dt.2007$DateTime)
+  
+}
+  
+# delete the source data set 
+if (file.exists('local data/household_power_consumption.txt')) {
+  x<-file.remove('local data/household_power_consumption.txt')
+}
+
+
+## Plot 3 - Line Graph of Energy sub Metering for each meter over time_interval
+
+# open device
+dev.copy(png, file = "plot3.png", width = 480, height = 480, units='px') # Copy my plot to a PNG file 
+
+# Plot line graph for each sub_meter and add legend
+
+with(dt.2007, plot(dt.2007$DateTime,dt.2007$Sub_metering_1,col = "black", type = "l", ylab = "Energy sub metering", xlab = ""))
+lines(dt.2007$DateTime,dt.2007$Sub_metering_2, col = "red")
+lines(dt.2007$DateTime,dt.2007$Sub_metering_3, col = "blue")
+legend("topright", lty = "solid", col = c("black", "red", "blue"), 
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+#close the PDF file device # Close the PNG device
+dev.off()  
+
+
+
+
+
+
